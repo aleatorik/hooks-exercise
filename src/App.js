84b482623992943b45d20f300a954947e4f44eba
daseulsong <vates0328@gmail.com>
2021-01-23 14:49:01 +0900

    Add useClick with useRef & useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    //fire the event below when componentWillUnMount is triggered
+    return () => element.current.removeEventListener("click", onClick);
+  }, []); // with [], EventListener will be executed once in componentDidMount. without [], the eventListener will be added everytime update is made.
+  return element;
+};
+
 function App() {
-  const input = useRef();
-  setTimeout(() => input.current.focus(), 3000);
-  // it's like getElementById()
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <h1>Hi</h1>
-      <input ref={input} placeholder="type" />
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
