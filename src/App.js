1345ded691960fbfd07b444ab115eb661c592fca
daseulsong <vates0328@gmail.com>
2021-01-23 10:33:56 +0900

    Add count

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 
 function App() {
-  return <div>hi</div>;
+  const [item, setItem] = useState(1);
+  const increment = () => setItem(item + 1);
+  const decrement = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>This is count {item}</h1>
+      <h2>Click any button below and see what happen</h2>
+      <button onClick={increment}>increse</button>
+      <button onClick={decrement}>decrese</button>
+    </div>
+  );
 }
 
 export default App;
