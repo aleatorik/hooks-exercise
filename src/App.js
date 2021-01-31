d23c809286e176a5c7d78eec427c8435c7654a83
daseulsong <vates0328@gmail.com>
2021-01-31 23:22:27 +0900

    Add eventPractice with setState hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import React, { useState, useEffect } from "react";
-import defaultAxios from "axios";
-
-const useAxios = (options, axiosInstance = defaultAxios) => {
-  const [state, setState] = useState({
-    loading: true,
-    error: null,
-    data: null,
-  });
-  const [trigger, setTrigger] = useState(0);
-  const refetch = () => {
-    setState({
-      ...state,
-      loading: true,
-    });
-    setTrigger(new Date());
-  };
-  useEffect(() => {
-    axiosInstance(options)
-      .then((data) => {
-        setState({
-          ...state,
-          loading: false,
-          data,
-        });
-      })
-      .catch((error) => {
-        setState({ ...state, loading: false, error });
-      });
-  }, [trigger]);
-  return { ...state, refetch };
-};
+import React from "react";
+import EventPractice from "./useState/eventPractice";
 
 function App() {
-  const { loading, data, error, refetch } = useAxios({
-    url:
-      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
-  });
   return (
     <div className="App">
-      <h1>{data && data.status}</h1>
-      <h2>{loading && "Loading"}</h2>
-      <button onClick={refetch}>Refetch></button>
+      <EventPractice />
     </div>
   );
 }
